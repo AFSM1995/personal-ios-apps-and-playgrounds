534f626f7fdd7e551cd1230f886bcfa9e8c5a9b8
Al <afsm1995@live.com>
2019-12-24 19:44:37 -0700

    Image View Experiment

---
+++ Switch/Switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Switch
+//
+//  Created by Álvaro Santillan on 11/20/18.
+//  Copyright © 2018 Álvaro Santillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
